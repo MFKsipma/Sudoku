d05f15aaa7451060316ca623375e4d71d2a67c9e
mfksipma <MFKsipma@gmail.com>
2025-04-02 13:27:57 +0200

    unlooped works

---
+++ sudoku.py
@@ -0,0 +1,106 @@
+#sudokuMap = [[["."] for i in range(9)] for j in range(9)]
+
+testPuzzle = [['8', '.', '.', '4', '.', '6', '.', '.', '7'],
+              ['.', '.', '.', '.', '.', '.', '4', '.', '.'],
+              ['.', '1', '.', '.', '.', '.', '6', '5', '.'],
+              ['5', '.', '9', '.', '3', '.', '7', '8', '.'],
+              ['.', '.', '.', '.', '7', '.', '.', '.', '.'],
+              ['.', '4', '8', '.', '2', '.', '1', '.', '3'],
+              ['.', '5', '2', '.', '.', '.', '.', '9', '.'],
+              ['.', '.', '1', '.', '.', '.', '.', '.', '.'],
+              ['3', '.', '.', '9', '.', '2', '.', '.', '5']]
+
+sudokuMap = testPuzzle
+
+def clone(inputList):
+    newlist = []
+    for i in inputList:
+        if type(i) == list:
+            newlist.append(clone(i))
+        else:
+            newlist.append(i)
+    return newlist
+
+def sudokuPrint(sudokuMap):
+    print("_ _ _ _ _ _ _ _ _ _ _ _ _")
+    for i in range(9):
+        if i == 3 or i == 6:
+            print("| - - - + - - - + - - - |")
+        line = "|"
+        for j in range(9):
+            if j == 3 or j == 6:
+                line+= " |"
+            line += " " + sudokuMap[i][j]
+        line += " |"
+        print(line)
+    print("‾ ‾ ‾ ‾ ‾ ‾ ‾ ‾ ‾ ‾ ‾ ‾ ‾")
+
+def vertical(sudokuMap):
+    verticalMap = []
+    for i in range(9):
+        verticalMap.append([])
+    for i in range(9):
+        for j in range(9):
+            verticalMap[i].append(sudokuMap[j][i])
+    return verticalMap
+
+def block(sudokuMap):
+    blockMap = []
+    for i in range(9):
+        blockMap.append([])
+    mapCounter = 0
+    for bigBlockY in range(3):
+        for bigBlockX in range(3):
+            for smallBlockY in range(3):
+                for smallBlockX in range(3):
+                    blockMap[mapCounter].append(sudokuMap[bigBlockY * 3 + smallBlockY][bigBlockX * 3 + smallBlockX])
+            mapCounter += 1
+    return blockMap
+
+def numberLogic(solveMap):
+    newSolveMap = clone(solveMap)
+    for Y in range(9):
+        for X in range(9):
+            for numbers in solveMap[Y][X]:
+                # test if the number is in horizontal rows
+                if numbers in testPuzzle[Y]:
+                    newSolveMap[Y][X].remove(numbers)
+                # test if the number is in vertical rows
+                elif numbers in verticalMap[X]:
+                    newSolveMap[Y][X].remove(numbers)
+                # test if the number is in blocks
+                elif numbers in blockMap[(X // 3) + ((Y // 3) * 3)]:
+                    newSolveMap[Y][X].remove(numbers)
+    return newSolveMap
+
+def mapFiller(sudokuMap, solveMap):
+    for horizontalRow in range(9):
+        for verticalRow in range(9):
+            if len(solveMap[horizontalRow][verticalRow]) == 1:
+                print(solveMap[horizontalRow][verticalRow])
+                sudokuMap[horizontalRow][verticalRow] = solveMap[horizontalRow][verticalRow][0]
+    return sudokuMap
+
+
+verticalMap = vertical(sudokuMap)
+blockMap = block(sudokuMap)
+solveMap = [[["1", "2", "3", "4", "5", "6", "7", "8", "9"] for i in range(9)] for j in range(9)]
+
+newSolveMap = numberLogic(solveMap)
+
+#for i in range(9):
+#    for j in range(9):
+#        print(newSolveMap[i][j])
+
+
+sudokuPrint(sudokuMap)
+
+
+mapFiller(sudokuMap, newSolveMap)
+sudokuPrint(sudokuMap)
+
+# test
+
+
+
+
